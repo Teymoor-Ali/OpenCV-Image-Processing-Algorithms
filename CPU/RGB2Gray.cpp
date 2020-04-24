4553e0d9e374521e7bae5c820aee0e8c2fbaa6d1
Teymoor <teymoor@hotmail.co.uk>
2020-04-24 10:56:01 +0100

    Updated RGB2GRAY

--- CPU/RGB2Gray.cpp
+++ CPU/RGB2Gray.cpp
@@ -1,50 +1,39 @@
-// RGB2YUV.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include<opencv2/opencv.hpp>
-#include<iostream>
+#include <iostream>
 #include <chrono> 
+#include "opencv2/opencv.hpp"
+
 using namespace std::chrono;
 using namespace std;
 using namespace cv;
-int main()
-{
-	try
-	{
 
-		Mat img = imread("mountain.png");
+int main(int argc, char* argv[])
+{		
+        auto start = chrono::high_resolution_clock::now();
+        cv::Mat h_img1 = cv::imread("Rainier.bmp");
+        //Define device variables
+        cv::cuda::GpuMat d_result1, d_img1;
+        //Upload Image to device
+        d_img1.upload(h_img1);
 
-		if (!img.data)
-		{
-			std::cout << "Image not loaded";
-			return -1;
-		}
-		auto start = high_resolution_clock::now();
-		
-		Mat RGB;
-		
-		cvtColor(img, RGB, COLOR_RGB2YUV);
-		
-		auto stop = high_resolution_clock::now();
-		
-		
-		namedWindow("Display window", COLOR_RGB2YUV);
-		imshow("Display window", img);
+        //Convert image to different color spaces
+        cv::cuda::cvtColor(d_img1, d_result1, cv::COLOR_RGB2GRAY);
 
-		namedWindow("Result window", WINDOW_AUTOSIZE);
-		imshow("Result window", RGB);
-		
-		auto duration = duration_cast<milliseconds>(stop - start);
-		
-		cout << "Time taken by function: " << duration.count() << " milliseconds" << endl;
-		
-		//waitKey();
-		
-		return 0;
-	}
-	catch (cv::Exception & e)
-	{
-		cerr << e.msg << endl; // output exception message
+
+        cv::Mat h_result1, h_result2, h_result3, h_result4;
+        //Download results back to host
+        d_result1.download(h_result1);
+        
+        auto end = chrono::high_resolution_clock::now();
+        cout << "Elapsed time in nanoseconds : "
+            << chrono::duration_cast<chrono::nanoseconds>(end - start).count() * 10e-6
+            << " ms" << endl;
+
+        
+        cv::imshow("Result in Gray ", h_result1);
+        waitKey();
+
+	
+	return 0;
+
 	}
 
-}
