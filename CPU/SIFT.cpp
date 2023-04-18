5203250b3a65cc165c4d1cb643b2dfc8e39ea6d5
Teymoor-Ali <teymoor@hotmail.co.uk>
2023-04-18 12:58:27 +0100

    Added detailed timings

--- CPU/SIFT.cpp
+++ CPU/SIFT.cpp
@@ -1,38 +1,67 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include "opencv2/xfeatures2d.hpp"
-#include "opencv2/xfeatures2d/nonfree.hpp"
-#include<iostream>
-#include <chrono> 
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
-using namespace std::chrono;
 using namespace cv;
 using namespace std;
 
-int main(int argc, const char* argv[])
+int main(int argc, char** argv)
 {
+    // Load two images
+    Mat img1 = imread("Rainier.bmp");
+    Mat img2 = imread("Rainier.bmp");
 
-    const cv::Mat input = cv::imread("rgray8.bmp", 0); //Load as grayscale
-   
-  
-    cv::Ptr<cv::SiftFeatureDetector> detector = cv::SiftFeatureDetector::create();
+    if (img1.empty() || img2.empty())
+    {
+        cerr << "Failed to load input image(s)" << endl;
+        return -1;
+    }
 
-    std::vector<cv::KeyPoint> keypoints;
+    // Declare SIFT detector and descriptor
+    Ptr<Feature2D> sift = SIFT::create();
 
-    detector->detect(input, keypoints);
-    
-    cv::Mat desc;
-    detector->compute(input, keypoints, desc);
+    // Detect keypoints and compute descriptors for both images
+    vector<KeyPoint> kp1, kp2;
+    Mat desc1, desc2;
 
+    double start_time = (double)getTickCount();
+    sift->detectAndCompute(img1, noArray(), kp1, desc1);
+    double descriptor_generation_time = ((double)getTickCount() - start_time) / getTickFrequency();
 
-    // Add results to image and save.
-    cv::Mat output;
-    cv::drawKeypoints(input, keypoints, output);
-    cv::imwrite("sift_result.bmp", output);
+    start_time = (double)getTickCount();
+    sift->detectAndCompute(img2, noArray(), kp2, desc2);
+    descriptor_generation_time += ((double)getTickCount() - start_time) / getTickFrequency();
 
+    // Match descriptors
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    vector<vector<DMatch>> matches;
 
+    start_time = (double)getTickCount();
+    matcher->knnMatch(desc1, desc2, matches, 2);
+    double matching_time = ((double)getTickCount() - start_time) / getTickFrequency();
 
-    waitKey();
+    // Filter matches using Lowe's ratio test
+    vector<DMatch> good_matches;
+    const float ratio_threshold = 0.7f;
+    for (size_t i = 0; i < matches.size(); ++i)
+    {
+        if (matches[i][0].distance < ratio_threshold * matches[i][1].distance)
+            good_matches.push_back(matches[i][0]);
+    }
+
+    // Draw matches
+    Mat img_matches;
+    drawMatches(img1, kp1, img2, kp2, good_matches, img_matches);
+
+    // Show results
+    imshow("Matches", img_matches);
+
+    // Compute total time
+    double total_time = descriptor_generation_time + matching_time;
+    cout << "Descriptor generation time: " << descriptor_generation_time << " seconds" << endl;
+    cout << "Matching time: " << matching_time << " seconds" << endl;
+    cout << "Total time: " << total_time << " seconds" << endl;
+
+    waitKey(0);
 
     return 0;
 }
